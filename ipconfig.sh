ac3cf4bdc0bf4a791bc3c9bf5c981bbb37952c73
FlumeZ <35439776+xFlum3@users.noreply.github.com>
2022-02-24 19:30:27 +0200

    Add files via upload

--- ipconfig.sh
+++ ipconfig.sh
@@ -24,7 +24,7 @@ default=$(ip route show | head -n 1 | cut -d" " -f3)
 echo "Default Gateway: $default"
 
 #dhcp=$(cat /var/lib/dhcp/dhclient.leases | grep "dhcp-server" | tail -n1 | cut -d"-" -f3 | cut -d" " -f2 | cut -d ";" -f1)
-dhcp=$(ip r | head -n 1 | cut -d" " -f9)
+dhcp=$(ip r | head -n 1 | cut -d" " -f3)
 echo "DHCP Server IP: $dhcp"
 
 #dns=$(systemd-resolve --status | grep "DNS " | head -n 2 | tail -n 1 | cut -d":" -f2)
