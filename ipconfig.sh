32993f4054664bd2a37bba692df2ae8d1fe57cf1
FlumeZ <35439776+xFlum3@users.noreply.github.com>
2022-02-24 19:11:45 +0200

    Added more options for subnet mask display

--- ipconfig.sh
+++ ipconfig.sh
@@ -5,28 +5,52 @@ echo "* Linux IP-Config Command Made by AlonD *"
 echo "*****************************************"
 
 ip=$(ip a | grep "inet "| head -n 2 | tail -n 1 | cut -d"/" -f1 | cut -d' ' -f6)
-echo "IP Address = $ip"
+echo "IP Address: $ip"
 
 subnet=$(ip a | grep "inet " | head -n 2 | tail -n 1 | cut -d"/" -f2 | cut -d' ' -f1)
-if test $subnet = 24; then
-    echo "Subnet Mask : 255.255.255.0"
+if test $subnet = 23; then
+    echo "Subnet Mask: 255.255.254.0"
+elif test $subnet = 24; then
+    echo "Subnet Mask: 255.255.255.0"
+elif test $subnet = 16; then
+    echo "Subnet Mask: 255.255.0.0"
+elif test $subnet = 8; then
+   echo "Subnet Mask: 255.0.0.0"
 else
     echo "Cannot find subnet mask ip"
 fi
 
 default=$(ip route show | head -n 1 | cut -d" " -f3)
-echo "Default Gateway : $default"
+echo "Default Gateway: $default"
 
-dhcp=$(cat /var/lib/dhcp/dhclient.leases | grep "dhcp-server" | tail -n1 | cut -d"-" -f3 | cut -d" " -f2 | cut -d ";" -f1)
-echo "DHCP Server IP : $dhcp"
+#dhcp=$(cat /var/lib/dhcp/dhclient.leases | grep "dhcp-server" | tail -n1 | cut -d"-" -f3 | cut -d" " -f2 | cut -d ";" -f1)
+dhcp=$(ip r | head -n 1 | cut -d" " -f9)
+echo "DHCP Server IP: $dhcp"
 
-dns=$(systemd-resolve --status | grep "DNS " | head -n 2 | tail -n 1 | cut -d":" -f2)
-echo "DNS Server : $dns"
+#dns=$(systemd-resolve --status | grep "DNS " | head -n 2 | tail -n 1 | cut -d":" -f2)
+dns=$(systemd-resolve --status | grep "Current" | tail -n 1 | cut -d" " -f6)
+echo "DNS Server: $dns"
 
 ipv6=$(ip a | grep "inet6 " | head -n 2 | tail -n 1 | cut -d"/" -f1 | cut -d' ' -f6)
 echo "IPV6: $ipv6"
 
+wired=$(ip a | grep "2: " | cut -d":" -f2 | cut -d" " -f2)
+echo "Wired Interface: $wired"
+
+date=$(date)
+echo "Date: $date"
+
+tz=$(timedatectl | grep "Time " | cut -d":" -f2)
+echo "Time Zone:$tz"
+
+lang=$(locale | head -n 1 | cut -d"=" -f2 | cut -d"." -f1)
+echo "Language: $lang"
+
 #PiHole Users!!!!!
-#pihole=$(lxc list | grep "(eth0) " | cut -d"|" -f4| cut -d" " -f2)
-#echo "PiHole IP: $pihole"
+pihole=$(lxc list | grep "(eth0) " | cut -d"|" -f4 | cut -d" " -f2)
+if test $pihole != $pihole; then
+	echo "PiHole Cannot be found"
+else
+	echo "PiHole IP: $pihole"
+fi
 exit 0
